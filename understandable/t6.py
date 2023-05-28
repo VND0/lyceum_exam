992cfc03c3c877ad821bbfcf2ed26ec682e1b916
user <varlamov577123@gmail.com>
2023-05-28 18:48:09 +0300

    Таки-решил шестое

--- understandable/t6.py
+++ understandable/t6.py
@@ -1,3 +1,14 @@
-# хз как это делать без списков
-# UPD:
-# я спросил у учителя: она сказала просто перебирать if-ами. Мне лень писать это.
+n1, n2, n3 = int(input()), int(input()), int(input())
+counter = 0
+
+if n1 == n2:
+    counter += 1
+if n1 == n3:
+    counter += 1
+if n2 == n3:
+    counter += 1
+
+if counter != 0 and counter != 3:
+    counter += 1
+
+print(counter)
