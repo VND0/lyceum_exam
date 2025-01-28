a9cd4fc81e74ee8cadb6f8c36bebc82de3958c2f
nikita <vnd-08@yandex.ru>
2025-01-28 22:17:59 +0300

    Updated README.md. Removed difficult solutions. Added some comments.

--- understandable/t3.py
+++ understandable/t3.py
@@ -5,7 +5,7 @@ y1 = int(input())
 x2 = int(input())
 y2 = int(input())
 
-
+# Формула стандартная, как для вектора
 distance = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 print("Расстояние между двумя точками ≈ " + str(distance))
